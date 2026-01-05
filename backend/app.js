f42b3af1581dc29738c91dbd4b0bc8550faa3b4c
Aditya <adishelke10@gmail.com>
2026-01-06 03:40:46 +0530

    Minor Changes

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const {readdirSync}=require('fs');
 require('dotenv').config()
 const db=require('./db/db');
 
-const PORT=process.env.PORT;
+const PORT=process.env.PORT || 8080;
 
 const app=express();
 
