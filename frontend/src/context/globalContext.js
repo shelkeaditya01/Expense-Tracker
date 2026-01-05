990deea7b5215818aa2e89839cbaffa3c44eefb7
Aditya <adishelke10@gmail.com>
2026-01-06 03:58:02 +0530

    Update globalContext.js

--- frontend/src/context/globalContext.js
+++ frontend/src/context/globalContext.js
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from "react";
 import axios from 'axios';
 
-const BASE_URL="http://localhost:8080/api/v1/";
+const BASE_URL="https://expense-tracker-gekr.onrender.com/api/v1/";
 
 const GlobalContext = React.createContext()
 
